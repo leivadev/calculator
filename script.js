452174641f8d592b588e68656edfa047d634163a
Luislev <luis.leiva@utp.ac.pa>
2023-12-14 23:43:57 -0500

    create functions and fix code

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ const btnOperators = document.querySelectorAll('.btn-operator');
 const btnClear = document.getElementById('btnClear');
 const btnRemove = document.getElementById('btnRemove');
 const btnCalculate = document.getElementById('btnCalculate');
+const btnDot = document.getElementById('btnDot');
 const screenCalculator = document.getElementsByClassName('screen');
 const screenHistory = document.getElementById('screen-history');
 const screenInput = document.getElementById('screen-input');
+
+let firstOperand = '';
+let secondOperand = '';
+let currentOperator = '';
 
 btnNumbers.forEach((button) =>
     button.addEventListener('click', () => appendNumber(button.textContent))
@@ -20,32 +25,71 @@ btnOperators.forEach((button) =>
 window.addEventListener('keydown', keyboardInput)
 btnDot.addEventListener('click', appendDot);
 btnClear.addEventListener('click', clearScreen);
-btnRemove.addEventListener('click', removeElement);
+btnRemove.addEventListener('click', removeNumber);
 btnCalculate.addEventListener('click', calculateOperation);
 
 function keyboardInput(e) {
     if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
     if (e.key === '.') appendDot()
-    if (e.key === '=' || e.key === 'Enter') evaluate()
-    if (e.key === 'Backspace') deleteNumber()
-    if (e.key === 'Escape') clear()
+    if (e.key === '=' || e.key === 'Enter') calculateOperation()
+    if (e.key === 'Backspace') removeNumber()
+    if (e.key === 'Escape') clearScreen()
     if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
-        appendOperator(convertOperator(e.key))
+        appendOperator(e.key)
 }
 
 function appendDot() {
     if (screenInput.textContent === '') screenInput.textContent = '0';
-    if (screenInput.textContent.includes('.')) return screenInput.textContent += '.';
+    if (!screenInput.textContent.includes('.')) return screenInput.textContent += '.';
 }
 
-function removeElement() {
+function removeNumber() {
     screenInput.textContent = screenInput.textContent.toString().slice(0, -1);
 }
 
 function clearScreen() {
     screenInput.textContent = '0';
+    screenHistory.textContent = '';
+    firstOperand = '';
+    secondOperand = '';
 }
 
 function appendNumber(number) {
+    if (screenInput.textContent === '0') screenInput.textContent = '';
     screenInput.textContent += number;
+}
+
+function appendOperator(operator) {
+    if (currentOperator !== null) calculateOperation()
+    if (screenInput.textContent === '') return;
+    firstOperand = screenInput.textContent;
+    screenHistory.textContent = `${firstOperand}${operator}`;
+    screenInput.textContent = '';
+}
+
+const add = (x, y) => x + y;
+const substract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+function operate(operator, x, y) {
+    x = Number(x);
+    y = Number(y);
+    switch (operator) {
+        case '+':
+            return add(x, y);
+        case '-':
+            return substract(x, y);
+        case '*':
+            return multiply(x, y);
+        case '/':
+            if (b === 0) return null;
+            return divide(x, y);
+        default:
+            return null;
+    }
+}
+
+function calculateOperation() {
+
 }
