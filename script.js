669a68fd5e5b38831598026727d46198e8da0a7d
Luislev <luis.leiva@utp.ac.pa>
2023-12-14 20:05:41 -0500

    add starter elements

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const btnNumbers = document.querySelectorAll('.btn-number');
+const btnOperators = document.querySelectorAll('.btn-operator');
+const btnClear = document.getElementById('btnClear');
+const btnRemove = document.getElementById('btnRemove');
+const btnCalculate = document.getElementById('btnCalculate');
+const screenCalculator = document.getElementsByClassName('screen');
+const screenHistory = document.getElementById('screen-history');
+const screenInput = document.getElementById('screen-input');
