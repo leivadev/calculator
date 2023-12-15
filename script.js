e2312a9a31e7161a4b55298a617f4749ddd1b8d3
Luislev <luis.leiva@utp.ac.pa>
2023-12-14 20:06:35 -0500

    create addEventListener for all interactions

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ const btnCalculate = document.getElementById('btnCalculate');
 const screenCalculator = document.getElementsByClassName('screen');
 const screenHistory = document.getElementById('screen-history');
 const screenInput = document.getElementById('screen-input');
+
+btnNumbers.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+btnOperators.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+window.addEventListener('keydown', keyboardInput)
+btnDot.addEventListener('click', appendDot);
+btnClear.addEventListener('click', clearScreen);
+btnRemove.addEventListener('click', removeElement);
+btnCalculate.addEventListener('click', calculateOperation);
