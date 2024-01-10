1f008445a784cdf639dbeb217a8ddc9c19f1ae9e
Luislev <luis.leiva@utp.ac.pa>
2024-01-09 23:20:05 -0500

    create theme toggle functionality

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//calculator functionality
+
 const btnNumbers = document.querySelectorAll('.btn-number');
 const btnOperators = document.querySelectorAll('.btn-operator');
 const btnClear = document.getElementById('btnClear');
@@ -109,3 +111,34 @@ function calculateOperation() {
     screenHistory.textContent = `${firstOperand} ${currentOperator} ${secondOperand} =`;
     currentOperator = null;
 }
+
+//theme mode toggle functionality
+
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+const button = document.querySelector("[data-theme-toggle]");
+const iconElement = document.getElementById("icon");
+
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+    if (localStorageTheme !== null) {
+        return localStorageTheme;
+    }
+
+    if (systemSettingDark.matches) {
+        return "dark";
+    }
+
+    return "light";
+}
+
+button.addEventListener("click", () => {
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+    const newIcon = newTheme === "dark" ? "fa-solid fa-moon" : "fa-solid fa-sun";
+    iconElement.className = newIcon;
+    document.querySelector("html").setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+
+    currentThemeSetting = newTheme;
+});
